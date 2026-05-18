0bdaffdeae91e56098822e04a0c93f091932e1c5
urbancruiseit <uchoit01@gmail.com>
2026-05-18 18:59:41 +0530

    update realtime

--- frontend/src/app/socket/index.ts
+++ frontend/src/app/socket/index.ts
@@ -1,7 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
-const SOCKET_URL =
-  process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:4000";
+const SOCKET_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000";
 
 let socket: Socket | null = null;
 

9c57f6c290040e85f5df6a8e74cb0f8ac1bf773a
urbancruiseit <uchoit01@gmail.com>
2026-07-20 12:43:58 +0530

    update route

--- backend/src/module/countrycode/country.route.js
+++ backend/src/module/countrycode/country.route.js
@@ -8,7 +8,6 @@ import { verifyJWT } from "../../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.use(verifyJWT);
 router.route("/").post(addCountryCode).get(getCountryCode);
 router.route("/codes").get(AllCountryCodes);
 
